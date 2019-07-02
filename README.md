c2859d8fdbe51cc2231bf4331a3d63d08cad6883
Greg Zahora <gregzahora3@gmail.com>
2019-07-02 16:34:49 -0400

    Added readme file and game instructions to the html

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# unit-4-game
+# Crystal Collector Game
+
+This game asks the player to attempt to match their score to the target score by clicking on the crystals. The catch is that the value of each gem is hidden, so the player will need to determine each crystal's value for the best chance at winning. It was created using a combination of html, javascript, bootstrap, and some light css.
+
+
+## Getting Started
+
+ 1. Each crystal has a hidden value
+ 2. Click the crystal and your score will increase by the hidden value assigned
+ 3. The goal is to make your score match the target score 
+ 4. If you accomplish this, you win the round
+ 4. If your score goes above the target score, you lose the round.
+ 5. After you win/lose the game will reset with a new target and new hidden values for each crystal.
+
+####### Tips: 
+-- "Your Score" will increase each time you select a crystal. Try to figre out the value of each crystal before attempting to match the score
+-- It may help to have a calculator nearby if math isn't your strong suit
+-- ** Hint: No crystal will have a value above 12 **
+
+
+## Author
+
+* **Greg Zahora** 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,14 @@
-//crystal values
+//crystal variables
 var blueCrystal = 0;
 var orangeCrystal = 0;
 var greenCrystal = 0;
 var redCrystal = 0;
 
-//target amount and your total
+//target and score variables
 var targetScore = 0;
 var yourScore = 0;
 
-//keeps track of the # of wins and losses
+//variables to keep track of wins and losses
 var wins = 0;
 var losses = 0;
 
@@ -19,15 +19,41 @@ function crystalValue() {
 
 //resets the game after a win or loss
 function resetGame() {
+    
+    //resets all values
     yourScore = 0;
     targetScore = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
     blueCrystal = crystalValue();
     orangeCrystal = crystalValue();
     greenCrystal = crystalValue();
     redCrystal = crystalValue();
+    
+    //displays values
     $("#targetScore").html(targetScore);
     $("#yourScore").html(yourScore);
+    $("#wins").html(wins);
+    $("#losses").html(losses);
 
+    //records new crystal values
+    console.log("Blue: " + blueCrystal);
+    console.log("Orange: " + orangeCrystal);
+    console.log("Green: " + greenCrystal);
+    console.log("Red: " + redCrystal);
+    console.log("-------------------------------------------------------")
+}
+
+//determines what to do if you've won or lost
+function winLose() {
+    if (yourScore === targetScore) {
+        wins++;
+        alert("You Win! Try again!");
+        resetGame();
+    }
+    else if (yourScore > targetScore) {
+        losses++;
+        alert("You Lose! Try again!")
+        resetGame();
+    }
 }
 
 
@@ -36,28 +62,27 @@ function resetGame() {
 
 resetGame();
 
-console.log("Blue: " + blueCrystal);
-console.log("Orange: " + orangeCrystal);
-console.log("Green: " + greenCrystal);
-console.log("Red: " + redCrystal);
-
 //click buttons
 $("#blueCrystal").click(function(){
     yourScore = yourScore + blueCrystal;
     $("#yourScore").html(yourScore);
+    winLose();
 });
 
 $("#orangeCrystal").click(function(){
     yourScore = yourScore + orangeCrystal;
     $("#yourScore").html(yourScore);
+    winLose();
 });
 
 $("#greenCrystal").click(function(){
     yourScore = yourScore + greenCrystal;
     $("#yourScore").html(yourScore);
+    winLose();
 });
 
 $("#redCrystal").click(function(){
     yourScore = yourScore + redCrystal;
     $("#yourScore").html(yourScore);
+    winLose();
 });
